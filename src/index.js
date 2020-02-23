e59c022d12702fb5303ffd36f29eaa67f6010e86
Roman Tymchenko <romatimchenko1984@gmail.com>
2020-02-23 14:40:22 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (Array.isArray(array) && array.length > 0) return Math.min(...array)
+  return 0
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (Array.isArray(array) && array.length > 0) return Math.max(...array)
+  return 0
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (Array.isArray(array) && array.length > 0) return array.reduce((sum,cur) => (sum + cur),0) / array.length
+  return 0
 }
+
